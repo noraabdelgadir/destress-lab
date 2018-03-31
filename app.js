a457399a06f47552854238ede63ca9effcf99d69
Nora Abdelgadir <nora.abdelgadir@mail.utoronto.ca>
2018-03-31 19:26:13 -0400

    moved music into music folder

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({secret: 'Secret cookie!'}));
 
-// var routes = require('./src/routes/index-routes');
-// app.use('', routes);
 
 /*  Page loading    */
 app.get('/', (req, res) => {
@@ -35,18 +33,18 @@ app.get('/images/patrick.jpg', (req, res) => {
     ms.pipe(req, res, './src/assets/images/patrick.jpg');
 });
 
-app.get('/login', (req, res) => {
-    res.sendFile('./src/pages/login.html', {root: __dirname});
-});
+// app.get('/login', (req, res) => {
+//     res.sendFile('./src/pages/login.html', {root: __dirname});
+// });
+//
+// app.get('/signup', (req, res) => {
+//     res.sendFile('./src/pages/signup.html', {root: __dirname});
+// });
 
-app.get('/signup', (req, res) => {
-    res.sendFile('./src/pages/signup.html', {root: __dirname});
-});
-
-app.get('/games', (req, res) => {
-    // Check for session here
-    res.sendFile('./src/pages/games.html', {root: __dirname});
-});
+// app.get('/games', (req, res) => {
+//     // Check for session here
+//     res.sendFile('./src/pages/games.html', {root: __dirname});
+// });
 
 app.get('/game/pop', (req, res) => {
     res.sendFile('./src/pages/pop-game.html', {root: __dirname});
@@ -58,7 +56,7 @@ app.get('/game/shooter', (req, res) => {
 
 /*  Game API    */
 app.get('/music/bensound-jazzcomedy.mp3', (req, res) => {
-    ms.pipe(req, res, './music/bensound-jazzcomedy.mp3');
+    ms.pipe(req, res, './src/assets/music/bensound-jazzcomedy.mp3');
 });
 
 app.get('/music/bensound-thejazzpiano.mp3', (req, res) => {
